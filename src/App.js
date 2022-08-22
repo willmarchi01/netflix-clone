15a3360f45185541afbe52d16d68623af7c060b7
Willian Marchi <willian.m.marchi@gmail.com>
2022-08-22 10:55:46 -0300

    NavBar Links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
+import { AuthContextProvider } from "./context/AuthContext";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Account from "./pages/Account"
 
 function App() {
   return (
     <>
-    <Navbar />
-    <Routes>
-      <Route path='/' element={<Home />} />
-    </Routes>
+    <AuthContextProvider>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/account' element={<Account />} />
+      </Routes>
+    </AuthContextProvider>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className='flex items-center justify-between p-2 z-[100] absolute w-full h-12 mt-3'>
+      <Link to='/'>
         <h1 className='text-green-600 text-4xl font-bold cursor-pointer ml-6'>TELEFLIX</h1>
+      </Link>
         <div className='mr-6'>
+          <Link to='/login'>
             <button className='text-white pr-4'>Sign In</button>
+          </Link>
+          <Link to='/signup'>
             <button className='bg-green-600 px-6 py-2 rounded cursor-pointer text-white hover:rounded-lg ease-in duration-200'>Sign Up</button>
+          </Link>
         </div>
     </div>
   )
